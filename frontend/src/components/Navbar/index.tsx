3a78417d3a6080a067b9d56224fdfcf1a8150eaa
edivandosevero05 <edivandosevero05@gmail.com>
2022-01-13 15:40:55 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import './styles.css';
+import {ReactComponent as GithubIcon} from '../../assets/img/github.svg';
+
+export default function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/edivandosevero05" target="_blank" rel="noreferrer">
+                        <div className="dsmovie-contact-container">
+                            <GithubIcon />
+                            <p className="dsmovie-contact-link">/edivandosevero</p>
+                        </div>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
