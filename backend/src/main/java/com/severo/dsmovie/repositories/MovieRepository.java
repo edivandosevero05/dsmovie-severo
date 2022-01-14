e305fb46e3ee3a63db60fc734d6906ef5f214da1
edivandosevero05 <edivandosevero05@gmail.com>
2022-01-14 16:23:54 -0300

    Find movies

---
+++ backend/src/main/java/com/severo/dsmovie/repositories/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.severo.dsmovie.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.severo.dsmovie.entities.Movie;
+
+public interface MovieRepository extends JpaRepository<Movie, Long>{
+
+}
